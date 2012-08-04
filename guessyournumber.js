e35fc60fc71ef1df645b7b1d2430d032fffa2868
Sam Pullara <spullara@yahoo.com>
2012-08-04 11:54:40 -0700

    update with annas style

--- guessyournumber.js
+++ guessyournumber.js
@@ -1,25 +1,25 @@
 if (Meteor.is_client) {
   Session.set("guessing", false)
-  Session.set("done", false)
-  Session.set("message", "I'm going to guess your number")
+  Session.set("message", "I'm going to guess your number between 1 and 30")
+  Session.set("button", "Start")
 
   Template.start.message = function() {
 	return Session.get("message")
   }
+  Template.start.button = function() {
+    return Session.get("button")	
+  }
   Template.start.guessing = function() {
 	return Session.get("guessing")
   }
-  Template.start.done = function() {
-	return Session.get("done")
-  }
   Template.guess.number = function() {
-	return Math.round((Session.get("highest") - Session.get("lowest"))/2) + Session.get("lowest")
+	return Math.round( (Session.get("highest") - Session.get("lowest") ) / 2 ) + Session.get("lowest")
   } 
 
   Template.start.events = {
     'click #start' : function () {
 	  Session.set("lowest", 0)
-	  Session.set("highest", 101)
+	  Session.set("highest", 31)
 	  Session.set("guessing", true)
 	  Session.set("tries", 1)
     }
@@ -35,8 +35,9 @@ if (Meteor.is_client) {
 	  Session.set("highest", Template.guess.number())
     },
     'click #gotit': function() {
-	  Session.set("message", "Yay! I got it in " + Session.get("tries") + " tries.")
+	  Session.set("message", "I'm correct! I got it in " + Session.get("tries") + " tries.")
 	  Session.set("guessing", false)
+	  Session.set("button", "Play again?")	
     }
   }
 }
